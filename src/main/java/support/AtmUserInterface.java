71933afc4c192adccd27d816a62a9778bc00e578
Stuart Corcoran <stuart.corcoran@gmail.com>
2015-04-15 11:58:49 -0700

    updated AtmUserInterface
    started code to hook UI to model

--- src/main/java/support/AtmUserInterface.java
+++ src/main/java/support/AtmUserInterface.java
@@ -1,11 +1,29 @@
 package support;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
 import nicebank.Account;
 import nicebank.Teller;
 
 class AtmUserInterface implements Teller {
 	
+	private final EventFiringWebDriver webDriver;
+	
+	public AtmUserInterface() {
+		this.webDriver = new EventFiringWebDriver(new FirefoxDriver());
+	}
+	
 	public void withdrawFrom(Account account, int dollars) {
-		
+		try {
+			webDriver.get("http://localhost:9988");
+			webDriver.findElement(By.id("Amount")).
+				sendKeys(String.valueOf(dollars));
+			webDriver.findElement(By.id("withdraw")).click();
+		}
+		finally {
+			webDriver.close();
+		}
 	}
 }
